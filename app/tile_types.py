0c278f8a01b11322d0794acb696363a6a732aa9d
Dimitri <dimitrilw@gmail.com>
2021-11-21 11:16:54 -0700

    fix(tiles): tweak formatting

--- app/tile_types.py
+++ app/tile_types.py
@@ -31,8 +31,12 @@ def new_tile(
     return np.array((walkable, transparent, dark), dtype=tile_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False,
+    transparent=False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
 )
