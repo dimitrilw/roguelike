cc4cd6f1313b76d851fec1dc1183537047795b10
Dimitri <dimitrilw@gmail.com>
2021-11-21 11:08:45 -0700

    feat(engine): refactor main() to use Engine class

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@
 
 # BUILT-INS
 import warnings
-# PIP INSTALLED
+# INSTALLED
 import tcod
 # LOCALS
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -20,11 +20,13 @@ def main() -> None:
         charmap=tcod.tileset.CHARMAP_TCOD,
     )
 
-    event_handler = EventHandler()
+    event_handler = EventHandler() # TODO: move this next to engine
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -35,23 +37,9 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
-
-            context.present(root_console)
-
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy)
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     with warnings.catch_warnings():
