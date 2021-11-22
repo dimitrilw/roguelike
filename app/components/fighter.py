bcadb63151dd86cc644059f6cd9837dd904f12ef
Dimitri <dimitrilw@gmail.com>
2021-11-21 17:57:57 -0700

    add player death

--- app/components/fighter.py
+++ app/components/fighter.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 # LOCAL
 from components.base_component import BaseComponent
+from input_handlers import GameOverEventHandler
 from render_order import RenderOrder
 if TYPE_CHECKING:
     from entity import Actor
@@ -29,6 +30,7 @@ class Fighter(BaseComponent):
     def die(self) -> None:
         if self.engine.player is self.entity:
             death_message = "You died!"
+            self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.entity.name} is dead!"
 
