333ea0b29cc711390a9d6b86b971b9e1b7607b00
Dimitri <dimitrilw@gmail.com>
2021-11-21 22:21:06 -0700

    implement message_log

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import warnings
 # INSTALLED
 import tcod
 # LOCAL
+import color
 from engine import Engine
 import entity_factory
 from procgen import generate_dungeon
@@ -15,7 +16,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -43,6 +44,10 @@ def main() -> None:
         engine=engine,
     )
     engine.update_fov()
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!",
+        color.welcome_text,
+    )
 
     with tcod.context.new_terminal(
         screen_width,
