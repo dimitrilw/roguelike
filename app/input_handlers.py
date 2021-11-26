30826a51a7a4f4d4dad84641e64fa2049affbe55
Dimitri <dimitrilw@gmail.com>
2021-11-26 11:03:29 -0700

    add ability to use & drop items

--- app/input_handlers.py
+++ app/input_handlers.py
@@ -4,6 +4,7 @@ from typing import Optional, TYPE_CHECKING
 # INSTALLED
 import tcod.event
 # LOCAL
+import actions
 from actions import (
     Action,
     BumpAction,
@@ -199,6 +200,25 @@ class InventoryEventHandler(AskUserEventHandler):
         """Called when the user selects a valid item."""
         raise NotImplementedError()
 
+class InventoryActivateHandler(InventoryEventHandler):
+    """Handle using an inventory item."""
+
+    TITLE = "Select an item to use"
+
+    def on_item_selected(self, item: Item) -> Optional[Action]:
+        """Return the action for the selected item."""
+        return item.consumable.get_action(self.engine.player)
+
+
+class InventoryDropHandler(InventoryEventHandler):
+    """Handle dropping an inventory item."""
+
+    TITLE = "Select an item to drop"
+
+    def on_item_selected(self, item: Item) -> Optional[Action]:
+        """Drop this item."""
+        return actions.DropItem(self.engine.player, item)
+
 class MainGameEventHandler(EventHandler):
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
@@ -220,6 +240,12 @@ class MainGameEventHandler(EventHandler):
 
         elif key == tcod.event.K_g:
             action = PickupAction(player)
+
+        elif key == tcod.event.K_i:
+            self.engine.event_handler = InventoryActivateHandler(self.engine)
+        #elif key == tcod.event.K_d:
+        elif key == tcod.event.K_o:
+            self.engine.event_handler = InventoryDropHandler(self.engine)
 
         # No valid key was pressed
         return action
